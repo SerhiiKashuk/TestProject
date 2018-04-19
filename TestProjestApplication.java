4f83eb942c9b85dcd361fd17270dfd9d05ea4c6b
sergiolaza <kashuk.sergii@gmail.com>
2018-04-19 15:37:22 +0300

    Add files via upload

---
+++ TestProjestApplication.java
@@ -0,0 +1,12 @@
+package com.example.TestProjest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjestApplication.class, args);
+	}
+}
